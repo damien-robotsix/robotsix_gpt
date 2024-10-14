16e37eea227d29b93973744862d043a508b8f9df
Damien SIX <damien@robotsix.net>
2024-10-14 17:56:51 +0200

    chore: Remove unused import of time in repo_chunker.py

--- ai_assistant/repo_chunker.py
+++ ai_assistant/repo_chunker.py
@@ -3,7 +3,6 @@ from tqdm import tqdm
 import os
 import pandas as pd
 from pathlib import Path
-import time
 from ai_assistant.git_utils import find_git_root, load_gitignore, PathSpec, GitWildMatchPattern
 from tree_sitter_languages import get_parser
 from magika import Magika
